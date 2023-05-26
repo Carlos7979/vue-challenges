6c547d5f48cac2af49ba8cf1cebc299dd2d6dc51
Carlos7979 <carloscorredorwg@gmail.com>
2023-05-25 22:54:45 -0400

    add Counter challenge

--- desafio-01/app.js
+++ desafio-01/app.js
@@ -2,9 +2,28 @@ const { createApp } = Vue
 
 createApp({
     data() {
-        return {}
+        return {
+			title: 'Contador',
+			counter: 0,
+			coderImg: './img/coderhouse.png',
+			coderW: 50,
+			vueImg: './img/vue.png',
+			vueW: 50,
+			student: 'Carlos Corredor',
+			teacher: 'Brian Cuenca',
+			tutor: 'Christian Gigena',
+			subject: 'Vue JS',
+			commission: '40500'
+		}
     },
 	created() {},
-	methods: {},
+	methods: {
+		add() {
+			++this.counter
+		},
+		minus() {
+			--this.counter
+		}
+	},
 	computed: {}
 }).mount('#app')
